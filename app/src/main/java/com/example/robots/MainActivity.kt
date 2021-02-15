5c13d6fcfe294029e2fb7da235117baa0bb4ff0d
Haris Mexis <theoharismexis@hotmail.com>
2021-02-15 01:41:52 +0000

    first commit

---
+++ app/src/main/java/com/example/robots/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.robots
+
+import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.`object`.conversation.Say
+import com.aldebaran.qi.sdk.builder.SayBuilder
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
+
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        QiSDK.register(this, this)
+    }
+
+    override fun onDestroy() {
+        QiSDK.unregister(this, this)
+        super.onDestroy()
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext) {
+        val say: Say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("Hello human!") // Set the text to say.
+                .build() // Build the say action.
+
+        // Execute the action.
+        say.run()
+    }
+
+    override fun onRobotFocusLost() {
+        // The robot focus is lost.
+    }
+
+    override fun onRobotFocusRefused(reason: String) {
+        // The robot focus is refused.
+    }
+}
